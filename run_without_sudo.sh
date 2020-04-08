8038c38460c847a5efac3d7e8d08ecf52304aecf
Misael Cureño <curenosm@gmail.com>
2020-04-08 10:51:47 -0500

    NVM version modified and set alias command

--- run_without_sudo.sh
+++ run_without_sudo.sh
@@ -1,7 +1,11 @@
 ################# Node & global packages #############
 
-curl https://raw.github.com/creationix/nvm/master/install.sh | sh
-source ~/.nvm/nvm.sh
-nvm install node
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+# Cerrar sesión o ejecutar
+source ~/.profile
+nvm install 12.16.1
+nvm run 12.16.1
+nvm alias default 12.16.1
+
 npm install -g create-react-app
 
