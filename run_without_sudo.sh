edd4aac818193a81c9fc2cadaccb18a4105a75e6
Misael Cureño <curenosm@gmail.com>
2020-04-08 10:33:01 -0500

    Nothing changed

--- run_without_sudo.sh
+++ run_without_sudo.sh
@@ -1,4 +1,4 @@
-################### Node & global packages #############
+################# Node & global packages #############
 
 curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 source ~/.nvm/nvm.sh
