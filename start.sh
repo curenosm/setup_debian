1990f5bf6d64ef9331f9033534a2a1eacd5c0076
Misael Cureño <curenosm@gmail.com>
2020-04-09 12:30:45 -0500

    Script to install Gitlab Runner

--- start.sh
+++ start.sh
@@ -34,6 +34,7 @@ sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 docker-compose --version
 
+
 #################### VSCode #############################
 
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
---
+++ sudo_bash_gitlab-runner.sh
@@ -0,0 +1,14 @@
+# First we download the binaries
+wget -O /usr/local/bin/gitlab-runner https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64
+
+# Then we change the permissions
+chmod +x /usr/local/bin/gitlab-runner
+
+# We create the user
+useradd --comment 'GitLab Runner' --create-home gitlab-runner --shell /bin/bash
+
+# Installation
+gitlab-runner install --user=gitlab-runner --working-directory=/home/gitlab-runner
+
+# Then start the runner
+gitlab-runner start
