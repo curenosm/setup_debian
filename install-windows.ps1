ea08526ed4747ae54067f8bd02cf3e73661c5a67
Misael Cureño <curenosm@gmail.com>
2023-12-03 09:39:44 -0600

    Create install-windows.ps1

---
+++ install-windows.ps1
@@ -0,0 +1,9 @@
+# Download clj
+iwr -useb github.com/clojure/brew-install/releases/latest/download/win-install.ps1 | iex
+
+# Download lein
+choco install lein
+choco install unzip
+choco install maven
+choco install gradle
+choco install vim
