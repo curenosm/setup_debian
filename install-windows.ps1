8e1793eeda88bd8b69c7a0e80420ec19146e0fc3
Misael Cureño <curenosm@gmail.com>
2023-12-03 10:17:04 -0600

    Update install-windows.ps1

--- install-windows.ps1
+++ install-windows.ps1
@@ -7,3 +7,11 @@ choco install unzip
 choco install maven
 choco install gradle
 choco install vim
+
+# PowerShell Profile themes for oh-my-posh
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\tokyonight_storm.omp.json" | Invoke-Expression
+# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\1_shell.omp.json" | Invoke-Expression
+# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\clean-detailed.omp.json" | Invoke-Expression
+# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\di4am0nd.omp.json" | Invoke-Expression
+# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\emodipt-extend.omp.json" | Invoke-Expression
+# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\smoothie.omp.json" | Invoke-Expression
